72d1d3e45fdce0cdf6fe179a9bdbc941704e8f36
Chau Han <chaugiahan001@gmail.com>
2025-04-21 20:14:36 +0700

    update extra move && check game over

--- o-an-quan.cpp
+++ o-an-quan.cpp
@@ -33,7 +33,19 @@ void printBoard(int board[12], int score[2]) {
     cout << "    ]\n";
 
     // Quan row: pits 11 and 5 (both should be shown as value 10)
-    cout << "\t[ " << board[11] * 10 << " |========================| " << board[5] * 10 << " ]\n";
+//    cout << "\t[ " << board[11] * valuePerStone[11] 
+//     	 << " |========================| " 
+//     	 << board[5] * valuePerStone[5] << " ]\n";
+
+	auto calcQuanValue = [](int count) {
+	    if (count == 0) return 0;
+	    return 10 + (count - 1); // 10 for Quan, rest are 1-point stones
+	};
+	
+	cout << "\t[ " << calcQuanValue(board[11]) 
+	     << " |========================| " 
+	     << calcQuanValue(board[5]) << " ]\n";
+	
 
     // Bottom row: index 0 to 4
     cout << "\t[    |";
@@ -82,12 +94,15 @@ bool makeMove(int pit, int player) {
         i = (i + 1) % 12;
         
         // Skip opponent's big pit
-        if ((player == 1 && i == 11) || (player == 2 && i == 5))
+        if ((player == 1 && i == 11) || (player == 2 && i == 5)) {
         	continue;
+		}
         	
         board[i]++;
         stones--;
     }
+    
+    
     
     // Check if landed in your own stone
     if ((player == 1 && i == 5) || (player == 2 && i == 11)) {
@@ -119,6 +134,30 @@ bool makeMove(int pit, int player) {
     return false;	// No extra turn
 }
 
+bool isGameOver() {
+    bool player1Empty = true;
+    bool player2Empty = true;
+
+    for (int i = 0; i <= 4; ++i) {
+        if (board[i] != 0) player1Empty = false;
+    }
+    for (int i = 6; i <= 10; ++i) {
+        if (board[i] != 0) player2Empty = false;
+    }
+
+    return player1Empty || player2Empty;
+}
+
+void collectRemainStones() {
+    for (int i = 0; i <= 4; ++i) {
+        score[0] += board[i];
+        board[i] = 0;
+    }
+    for (int i = 6; i <= 10; ++i) {
+        score[1] += board[i];
+        board[i] = 0;
+    }
+}
 
 int main() {
 	int currentPlayer = 1;
@@ -129,16 +168,45 @@ int main() {
         switch (currentPlayer) {
             case 1: {
                 int pit = getValidMove(1);
-                makeMove(pit, 1);
-                currentPlayer = 2;
+                bool extraTurn = makeMove(pit, 1);
+                
+                if (!extraTurn) {
+                	cout << "Player 2 gets another turn!" << endl;  
+				}
+				else {
+					currentPlayer = 1;
+				}
+				
                 break;
             }
             case 2: {
-                int pit = getValidMove(2);
-                makeMove(pit, 2);
-                currentPlayer = 1;
+            	int pit = getValidMove(2);
+            	bool extraTurn = makeMove(pit, 2);
+                
+                if (!extraTurn) {
+                	cout << "Player 2 gets another turn!" << endl;  
+				}
+				else {
+					currentPlayer = 1;
+				}
+            
                 break;
             }
+        }
+        
+        
+        if (isGameOver()) {
+            cout << "Game over!\n";
+            collectRemainStones();
+            printBoard(board, score);
+            if (score[0] > score[1]) {
+                cout << "Player 1 wins!\n";
+            } else if (score[1] > score[0]) {
+                cout << "Player 2 wins!\n";
+            } else {
+                cout << "It's a tie!\n";
+            }
+            break;
         }
     }
 
